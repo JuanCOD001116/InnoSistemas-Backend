6efc8f14db7cbe89bfb3748bbdb1438450b59c53
JuanCOD001116 <160247221+JuanCOD001116@users.noreply.github.com>
2025-09-14 19:01:30 -0500

    inicialización del proyecto
    ...

---
+++ InnoSistemas/InnoSistemas/src/main/java/com/innosistemas/InnoSistemas/InnoSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.innosistemas.InnoSistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoSistemasApplication.class, args);
+	}
+
+}
